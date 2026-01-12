c5e2758771a84e4f1151d9010d0b38fa953da781
Ioannis Tsiros <itsiros@2-D-9.42heilbronn.de>
2026-01-12 17:03:56 +0100

    Implimenting json respond handler

--- main.go
+++ main.go
@@ -1,52 +1,78 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"structs"
 	"sync/atomic"
 )
 
-var serverIsBusy bool = false
-
-type apiConfig struct {
+type apiConf struct {
 	fileserverHits atomic.Int32
 }
 
-func healthzHandler(w http.ResponseWriter, r *http.Request) {
-
-	if serverIsBusy {
-		http.Error(w, "Service temporarily anavailable", http.StatusServiceUnavailable)
-		return
-	}
-
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
+func (cfg *apiConf) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		log.Printf("%s %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
 }
 
-func NewServer() *http.Server {
+func (cfg *apiConf) handlerHits(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	adminMetrics := fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", cfg.fileserverHits.Load())
+	w.Write([]byte(adminMetrics))
+}
 
-	mux := http.NewServeMux()
+func (cfg *apiConf) handlerResetHits(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	cfg.fileserverHits.Store(0)
+	hits := fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())
+	w.Write([]byte(hits))
+}
 
-	mux.HandleFunc("/healthz", healthzHandler)
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
 
-	fs := http.FileServer(http.Dir("."))
-	mux.Handle("/app/", http.StripPrefix("/app", fs))
+func hanlderJSonResponce(w http.ResponseWriter, r *http.Request) {
 
-	return &http.Server{
-		Addr:    ":8080",
-		Handler: mux,
+	var returnVals struct {
+		reqErr     string
+		valid      bool
+		StatusCode int
 	}
+	if len(r.Body) != 140 {
+
+	}
+	json.Marshal()
+	w.WriteHeader(http.StatusOK)
 }
 
 func main() {
+	const filepathRoot = "."
+	const port = "8080"
 
-	server := NewServer()
+	mux := http.NewServeMux()
+	cfg := &apiConf{}
+	mux.Handle("/app/", http.StripPrefix("/app", cfg.middlewareMetricsInc(http.FileServer(http.Dir(filepathRoot)))))
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /admin/metrics", cfg.handlerHits)
+	mux.HandleFunc("POST /admin/reset", cfg.handlerResetHits)
+	mux.HandleFunc("POST /api/validate_chirp", cfg.handlerResetHits)
 
-	fmt.Println("Server running on :8080")
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Println("Error: ", err)
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
 	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
 }
