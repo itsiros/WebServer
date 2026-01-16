fcb63996979c0d50a78025850e686205264d876a
Ioannis Tsiros <jtsiros93@gmail.com>
2026-01-16 19:51:32 +0100

    added simple migrations and queries for user database and an api call to register

--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 	"sync/atomic"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/tsironi93/WebServer/internal/database"
 )
 
 type apiConf struct {
 	fileserverHits atomic.Int32
+	db             *database.Queries
+	platform       string
 }
 
 type parameters struct {
@@ -37,6 +47,12 @@ func (cfg *apiConf) handlerHits(w http.ResponseWriter, r *http.Request) {
 }
 
 func (cfg *apiConf) handlerResetHits(w http.ResponseWriter, r *http.Request) {
+	if cfg.platform != "dev" {
+		respondWithError(w, 403, "Forbidden")
+		return
+	}
+
+	cfg.db.DeleteAllUsers(r.Context())
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	cfg.fileserverHits.Store(0)
@@ -101,18 +117,84 @@ func hanlderJSONResponce(w http.ResponseWriter, r *http.Request) {
 	respondJSON(&returnVals{params.Body}, w, 200)
 }
 
+func (cfg *apiConf) HandlerCreateUser(w http.ResponseWriter, r *http.Request) {
+	type createUser struct {
+		Email string `json:"email"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	create := createUser{}
+	if err := decoder.Decode(&create); err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		respondWithError(w, 500, "Something went wrong")
+		return
+	}
+
+	if !strings.Contains(create.Email, "@") || !strings.Contains(create.Email, ".") {
+		respondWithError(w, 401, "Wrong email format")
+		return
+	}
+
+	user, err := cfg.db.CreateUser(r.Context(), create.Email)
+	if err != nil {
+		log.Println("Error creating user:", err)
+		respondWithError(w, 500, "Something went wrong")
+		return
+	}
+
+	type userData struct {
+		Id        uuid.UUID `json:"id"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+		Email     string    `json:"email"`
+	}
+
+	resp := &userData{
+		Id:        user.ID,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+		Email:     user.Email,
+	}
+
+	dat, err := json.Marshal(resp)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		respondWithError(w, 500, "")
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(201)
+	w.Write(dat)
+}
+
 func main() {
+	godotenv.Load(".env")
+	dbURL := os.Getenv("DB_URL")
+	platform := os.Getenv("PLATFORM")
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	dbQueries := database.New(db)
+
 	const filepathRoot = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	cfg := &apiConf{}
+	cfg := &apiConf{
+		db:       dbQueries,
+		platform: platform,
+	}
 	mux.Handle("/app/", http.StripPrefix("/app", cfg.middlewareMetricsInc(http.FileServer(http.Dir(filepathRoot)))))
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 	mux.HandleFunc("GET /admin/metrics", cfg.handlerHits)
 	mux.HandleFunc("POST /admin/reset", cfg.handlerResetHits)
 	mux.HandleFunc("POST /api/validate_chirp", hanlderJSONResponce)
-
+	mux.HandleFunc("POST /api/users", cfg.HandlerCreateUser)
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
