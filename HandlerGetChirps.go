991d4f620651dfe63f9c310b687c17edc089c355
Ioannis Tsiros <jtsiros93@gmail.com>
2026-01-17 14:10:14 +0100

    done with get endpoint for chirps

--- HandlerGetChirps.go
+++ HandlerGetChirps.go
@@ -1,13 +1,51 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+)
 
 func (cfg *apiConf) handlerGetChirps(w http.ResponseWriter, r *http.Request) {
-	chirps, err := cfg.db.GetAllChirps(r.Context())
+	Allchirps, err := cfg.db.GetAllChirps(r.Context())
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Error getting chirps from database", err)
 		return
 	}
 
-	respondWithJSON(w, http.StatusOK, chirps)
+	resp := make([]struct {
+		ID        uuid.UUID `json:"id"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+		Body      string    `json:"body"`
+		UserID    uuid.UUID `json:"user_id"`
+	}, len(Allchirps))
+
+	for i, c := range Allchirps {
+		resp[i] = struct {
+			ID        uuid.UUID `json:"id"`
+			CreatedAt time.Time `json:"created_at"`
+			UpdatedAt time.Time `json:"updated_at"`
+			Body      string    `json:"body"`
+			UserID    uuid.UUID `json:"user_id"`
+		}{
+			ID:        c.ID,
+			CreatedAt: c.CreatedAt,
+			UpdatedAt: c.UpdatedAt,
+			Body:      c.Body,
+			UserID:    c.UserID,
+		}
+	}
+
+	dat, err := json.Marshal(resp)
+	if err != nil {
+		respondWithError(w, 500, "JSON encoding failed", err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	w.Write(dat)
 }
